13c6b2cd2e930dd67dea21e6332b3217ceb0ee0d
diadntjr <dntjr0501@gmail.com>
2022-04-01 01:04:40 +0900

    Add problem

---
+++ C++/bssm/day1/1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+	int a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0;
+	std::cin>>a1>>a2>>a3>>a4>>a5;
+	std::cout<<a1+a2+a3+a4+a5<<std::endl;
+	return 0;
+}
---
+++ C++/bssm/day1/2/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+	std::string data;
+	getline(std::cin, data);
+	std::cout<<data<<std::endl;
+	return 0;
+}
