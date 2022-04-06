c4bf5cda7460a905552d15731558c31fc36acaf1
diadntjr <dntjr0501@gmail.com>
2022-04-07 00:58:20 +0900

    Modified

--- JavaScript/day3/practice/5/index.js
+++ JavaScript/day3/practice/5/index.js
@@ -2,9 +2,17 @@ let string1 = "좋아하는 과일";
 let array1 = ["사과", "바나나", "딸기"]
 let string2 = "싫어하는 채소";
 let array2 = ["시금치", "오이"];
+let string = "";
 
 function generateSentence(desc, arr) {
-    console.log("제가 " + desc + " "+arr.length +"은 " + arr[0] + ", " + arr[1] + ", " + arr[2] + " 입니다.");
+    string += "제가 " + string1 + " "+ arr.length + "개는 ";
+    for(let i = 0; i<arr.length; i++) {
+        string += arr[i];
+        if(i != arr.length -1) string += ", "; 
+    }
+    string += "입니다.";
+    return string;
 }
 
-generateSentence(string1, array1);
+console.log(generateSentence(string1, array1));
+console.log(generateSentence(string2, array2));
