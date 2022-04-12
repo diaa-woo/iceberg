6d21e9753ce78e918602ee503f6e0e37629782c3
diadntjr <dntjr0501@gmail.com>
2022-04-12 14:44:51 +0900

    update

---
+++ Embeded/project/day5/sim_gpio.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "thread.h"
+
+// Task 1
+int gpio_pin_1_in_value_reg;  //0 or 1
+int gpio_pin_1_out_value_reg;  //0 or 1
+int gpio_pin_1_direct_reg = 1;  //0(Out) or 1(IN)
+
+void in()
+{
+    printf("IN: %d", gpio_pin_1_in_value_reg);
+}
+
+void out() 
+{
+    printf("OUT: %d", gpio_pin_1_out_value_reg);
+}
+
+void* gpio_pin_1(void* args) 
+{
+    while(1) {
+        if(gpio_pin_1_direct_reg) 
+        {
+            in();
+        }
+        else
+        {
+            out();
+        }
+    }
+}
+
+// Task 0(Main)
+int main() 
+{
+    uint32_t t_id;
+    t_id = thread_create(gpio_pin_1, 0);
+    while (1) {
+        int d, v;
+        scanf("%d", &d);  //direction
+        scanf("%d", &v);  //value
+        if(d)  {  //out_value_reg
+            gpio_pin_1_out_value_reg = v;
+        }
+        else {  //in_value_reg
+            gpio_pin_1_in_value_reg = v;
+        }
+    }
+    return 0;
+}
