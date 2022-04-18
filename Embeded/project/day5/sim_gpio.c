5b36215f3a017fb4c62c7177294530967f1b61f8
diadntjr <dntjr0501@gmail.com>
2022-04-19 02:16:29 +0900

    I don't know...

--- Embeded/project/day5/sim_gpio.c
+++ Embeded/project/day5/sim_gpio.c
@@ -9,22 +9,18 @@ int gpio_pin_1_direct_reg = 1;  //0(Out) or 1(IN)
 
 void in()
 {
-    printf("IN: %d", gpio_pin_1_in_value_reg);
+    fprintf(stderr, "IN: %d", gpio_pin_1_in_value_reg);
 }
 
 void out() 
 {
-    printf("OUT: %d", gpio_pin_1_out_value_reg);
+    fprintf(stderr, "OUT: %d", gpio_pin_1_out_value_reg);
 }
 
 void* gpio_pin_1(void* args) 
 {
-    int buffer = gpio_pin_1_direct_reg;
     while(1) 
     {
-        while(buffer == gpio_pin_1_direct_reg);
-        printf("%d", gpio_pin_1_direct_reg);
-        buffer = gpio_pin_1_direct_reg;
         if(gpio_pin_1_direct_reg) 
         {
             in();
@@ -44,13 +40,12 @@ int main()
     while (1) {
         int d, v;
         scanf("%d", &d);  //direction
+        gpio_pin_1_direct_reg = d;
         scanf("%d", &v);  //value
         if(d)  {  //out_value_reg
-            if(gpio_pin_1_direct_reg != 0) gpio_pin_1_direct_reg = 0;
             gpio_pin_1_out_value_reg = v;
         }
         else {  //in_value_reg
-            if(gpio_pin_1_direct_reg != 1) gpio_pin_1_direct_reg = 1;
             gpio_pin_1_in_value_reg = v;
         }
     }
