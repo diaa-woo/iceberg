48d65442bb009471965985057310f8d504e1f272
diadntjr <dntjr0501@gmail.com>
2022-05-16 22:43:45 +0900

    update

--- Embeded/project/day9/day9-complete.c
+++ Embeded/project/day9/day9-complete.c
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <stdint.h>
 
-uint32_t mem;  //이거 하나만 가지고 어떻게 야아구 겜을 만들 수 있을까....ㅠ
+uint32_t mem = 0;  //이거 하나만 가지고 어떻게 야아구 겜을 만들 수 있을까....ㅠ
 
 #define set_nibble(mem, index, value) (mem |= (value) << ((index) * 4)) 
 #define get_nibble(mem, index) ((mem >> (index * 4)) & 0xF)
@@ -25,30 +25,63 @@ void init_random(void)
     }
 }
 
+void reset_num(void)
+{
+    mem = mem << 24;
+    mem = mem >> 24;
+    return;
+}
+
 void input_num(void)
 {
-    int n[3];  //줄이기
-    scanf("%d %d %d", &n[0], &n[1], &n[2]);
-
-    for (int x, i = 0; i < 3; i++) {
-        set_nibble(mem, 3+i, n[i]);
+    uint8_t buf = 0;
+    for (int i = 3; i < 6; ++i) {
+        scanf("%hhd",&buf);
+        set_nibble(mem, i, buf);
     }
 }
+
+void process(void) {  //줄이기
+    for(int i=0;i<3;i++) {
+        for(int j=3;j<6;j++) {
+            printf("%d %d\n",i,j);
+            if(i==j-3) {
+                if(get_nibble(mem, i) == get_nibble(mem, j)) {
+                    set_nibble(mem, 6, (get_nibble(mem, 6)+0x01));
+                    printf("strike! %d\n", get_nibble(mem,6));
+                }
+            }
+            else {
+                if(get_nibble(mem, i) == get_nibble(mem, j)) {
+                    set_nibble(mem, 7, (get_nibble(mem, 7)+0x01));
+                    printf("ball %d\n", get_nibble(mem,7));
+                }
+            }
+        }
+    }
+    return;  
+}
+
 
 void display_mem(void)
 {
     for(int i = 0; i < 6; ++i) {
         printf("%d ", get_nibble(mem, i));
     }
-    printf("\n");
+    printf("\ns: %d b: %d\n", get_nibble(mem, 6), get_nibble(mem, 7));
+    return;
 }
 
 int main()
 {    
+    int i =0;
     init_random();
-    input_num();
-    display_mem();
-    
+    do {
+        reset_num();
+        input_num();
+        process();
+        display_mem();
+    }while(get_nibble(mem, 6)!=3);
     return 0;
 }
 
