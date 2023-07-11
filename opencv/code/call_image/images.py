e240e079ca536dc6e4141ae014d30af523245028
diadntjr <dev.dntjr0501@gmail.com>
2023-07-11 19:54:29 +0900

    Delete overlapped files
    opencv/

--- opencv/code/call_image/images.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import os
-
-os.chdir('.\code\call_image\\')
-image = cv2.imread('moon.jpg', flags=cv2.IMREAD_ANYCOLOR)
-cv2.imshow("Moon", image)
-cv2.waitKey()
-cv2.destroyAllWindows()
