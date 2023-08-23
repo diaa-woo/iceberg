911fb3a5750b30495722e50518e24a83014cd36e
diadntjr <dev.dntjr0501@gmail.com>
2023-08-23 20:12:43 +0900

    Add C# part
    EOM

---
+++ Csharp/test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            string data = "Hello World";
+            string[] datas = data.split('');
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
